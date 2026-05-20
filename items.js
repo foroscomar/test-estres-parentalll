5c91bcc7e532e1fe531787564a910a973df151d9
foroscomar <foroscom@autonoma.edu.pe>
2026-05-19 19:13:58 -0500

    Create items.js

---
+++ items.js
@@ -0,0 +1,52 @@
+const questions = [
+
+{
+id:1,
+dimension:"Malestar parental",
+reverse:false,
+text:"Las demandas relacionadas con la crianza de mi hijo/a me generan agotamiento físico y emocional."
+},
+
+{
+id:2,
+dimension:"Malestar parental",
+reverse:false,
+text:"Siento que las responsabilidades de cuidado superan mis recursos emocionales."
+},
+
+{
+id:3,
+dimension:"Malestar parental",
+reverse:false,
+text:"Me cuesta encontrar momentos de descanso debido a las necesidades de mi hijo/a."
+},
+
+{
+id:4,
+dimension:"Características del hijo",
+reverse:false,
+text:"Las conductas de mi hijo/a suelen ser emocionalmente difíciles de manejar."
+},
+
+{
+id:5,
+dimension:"Características del hijo",
+reverse:false,
+text:"Las dificultades emocionales de mi hijo/a generan tensión constante en el hogar."
+},
+
+{
+id:6,
+dimension:"Interacción padre-hijo",
+reverse:true,
+text:"Me siento emocionalmente conectado/a con mi hijo/a."
+},
+
+{
+id:7,
+dimension:"Interacción padre-hijo",
+reverse:false,
+text:"En ocasiones me siento distante emocionalmente de mi hijo/a."
+}
+
+];
